a8dbc25130a9d5398652769b24d63420103cfdb0
Ralfs Fadejevs <ralphfdesigns@gmail.com>
2024-03-20 17:54:19 +0100

    auth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -34,9 +34,16 @@ export async function Navbar() {
                 />
               ) : (
                 <div className="flex items-center gap-x-2">
-{/*                   <LoginLink>
+                  <LoginLink>
                     <Button className="sign-in-btn">Sign In</Button>
-                  </LoginLink> */}
+                  </LoginLink>
+
+                  {/* <Link
+                    href={`http://localhost3000/api/auth/kinde_callback`}
+                    className="get-started-button cursor-pointer rounded-md bg-none py-2 px-4 text-white"
+                  >
+                    Sign In
+                  </Link> */}
 
                   <Link
                     href={`https://costop.kinde.com/knock-knock`}
